dae95c878fffda404cd863f270022aefa6a54110
plysch <j.eric.persson@gmail.com>
2011-05-30 15:33:40 +0200

    fixed some minor bugs since merge

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,14 +7,14 @@ class PostsController < ApplicationController
   def show
     @comment = Comment.new
     @post = Post.find(params[:id])
-  end
-  
-  def new
-    @post = Post.new
     @map = GMap.new("map-" + @post.id.to_s)
     @map.control_init(:large_map => true,:map_type => true)
     @map.center_zoom_init([@post.longitude,@post.latitude],6)
     @map.overlay_init(GMarker.new([@post.longitude,@post.latitude],:title => @post.title, :info_window => @post.user.name))
+  end
+  
+  def new
+    @post = Post.new
   end
 
   def create
