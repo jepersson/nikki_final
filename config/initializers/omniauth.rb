71f6cbeb46ea1311fcaccbbb1491e46cd6f62aaa
plysch <j.eric.persson@gmail.com>
2011-06-10 01:41:03 +0200

    cleaned up directories

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '9d9e57b0ab26c68676b416bd799a719a', '0e509e799d502471370f3f2f5b2b49bc'
-  provider :twitter, 'mWEoLrc20zYgvzOwVuynWg', 'HpsFpVSnyg2CD1Yedn6jqa6vfbgKtPLN3DgxN0SsVU'
 end
